44273f32662da8ee00d8afdc2e198f98b1141d0a
Anton Yakunov <yakunov94@gmail.com>
2017-08-28 01:02:37 +0700

    Валидация формы

--- js/map.js
+++ js/map.js
@@ -292,9 +292,9 @@ var openDialog = function (ad) {
  * @param {string} className - Удаляемый класс
  */
 var removeClass = function (elems, className) {
-  elems.forEach(function (elem) {
-    elem.classList.remove(className);
-  });
+  for (var i = 0; i < elems.length; i++) {
+    elems[i].classList.remove(className);
+  }
 };
 
 /**
@@ -310,3 +310,83 @@ var deactivateDialogAndPin = function (evt) {
 
 dialogClose.addEventListener('click', deactivateDialogAndPin);
 document.body.addEventListener('keydown', deactivateDialogAndPin);
+
+var PRICE = {
+  'flat': 1000,
+  'bungalo': 0,
+  'house': 5000,
+  'palace': 10000
+};
+
+var CAPACITY_NUMBER = {
+  '1': ['1'],
+  '2': ['1', '2'],
+  '3': ['1', '2', '3'],
+  '100': ['0']
+};
+
+var form = document.querySelector('.notice__form');
+var titleField = form.querySelector('#title');
+var timeInField = form.querySelector('#timein');
+var timeOutField = form.querySelector('#timeout');
+var flatTypeField = form.querySelector('#type');
+var priceField = form.querySelector('#price');
+var roomNumberField = form.querySelector('#room_number');
+var capacityField = form.querySelector('#capacity');
+var options = capacityField.querySelectorAll('option');
+var formElems = form.querySelectorAll('input:not([type="submit"]), select');
+
+/**
+ * Синхронизирует цену с типом жилья
+ */
+var syncFlatWithPrice = function () {
+  priceField.min = PRICE[flatTypeField.value];
+};
+
+/**
+ * Синхронизирует количество гостей с количеством комнат
+ */
+var syncRoomsWithGuests = function () {
+  for (var i = 0; i < options.length; i++) {
+    options[i].disabled = !CAPACITY_NUMBER[roomNumberField.value].includes(options[i].value);
+    if (!options[i].disabled) {
+      capacityField.value = options[i].value;
+    }
+
+  }
+};
+
+/**
+ * Синхронизирует время выезда со временем заезда
+ */
+var syncTimeIn = function () {
+  timeOutField.selectedIndex = timeInField.selectedIndex;
+};
+
+/**
+ * Синхронизирует время заезда со временем выезда
+ */
+var syncTimeOut = function () {
+  timeInField.selectedIndex = timeOutField.selectedIndex;
+};
+
+titleField.addEventListener('input', function () {
+  titleField.setCustomValidity(
+      titleField.validity.patternMismatch ?
+        'Пожалуйста, используйте не менее 30 символов' :
+        ''
+  );
+});
+
+timeInField.addEventListener('change', syncTimeIn);
+timeOutField.addEventListener('change', syncTimeOut);
+
+roomNumberField.addEventListener('change', syncRoomsWithGuests);
+
+flatTypeField.addEventListener('change', syncFlatWithPrice);
+
+form.addEventListener('invalid', function () {
+  formElems.forEach(function (elem) {
+    elem.classList.toggle('invalid', !elem.validity.valid);
+  });
+}, true);
