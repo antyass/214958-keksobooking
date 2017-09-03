dd9ab1ab71a4842eaaa54b20b1250811282eeffa
Anton Yakunov <yakunov94@gmail.com>
2017-09-04 00:13:56 +0700

    Максимум подвижности

--- js/map.js
+++ js/map.js
@@ -3,10 +3,27 @@
 window.map = (function () {
 
   var NUMBER_OF_ADS = 8;
+  var MAIN_PIN_SIZE = {
+    WIDTH: 75,
+    HEIGHT: 94
+  };
+  var LOCATION = {
+    X: {
+      MIN: 300,
+      MAX: 900
+    },
+    Y: {
+      MIN: 100,
+      MAX: 500
+    }
+  };
   var ads = window.data.getAds(NUMBER_OF_ADS);
   var pins = window.pin.createPins(ads);
   var pinMap = document.querySelector('.tokyo__pin-map');
   pinMap.appendChild(pins);
+  var pinMain = pinMap.querySelector('.pin__main');
+  var form = document.querySelector('.notice__form');
+  var addressField = form.querySelector('#address');
 
   /**
    * Запускает функцию открытия диалога при активации пина
@@ -22,5 +39,83 @@ window.map = (function () {
   window.card.onDeactivate = function () {
     window.pin.deactivate();
   };
+
+  /**
+   * Синхронизирует координаты поля с пином
+   * @param {number} x
+   * @param {number} y
+   */
+  var syncFieldWithPin = function (x, y) {
+    addressField.value = 'x: ' + parseInt(x, 10) + ', y: ' + parseInt(y, 10);
+  };
+
+  /**
+   * Синхронизирует координаты пина с полем
+   */
+  var syncPinWithField = function () {
+    var coords = addressField.value.match(/\d+/g);
+
+    var coordX = checkLimit(parseInt(coords[0], 10), LOCATION.X.MIN, LOCATION.X.MAX);
+    var coordY = checkLimit(parseInt(coords[1], 10), LOCATION.Y.MIN, LOCATION.Y.MAX);
+
+    setPinCoords(coordX, coordY);
+  };
+
+  /**
+   * Устанавливает положение пина на карте
+   * @param {number} x
+   * @param {number} y
+   */
+  var setPinCoords = function (x, y) {
+    pinMain.style.left = x - MAIN_PIN_SIZE.WIDTH / 2 + 'px';
+    pinMain.style.top = y - MAIN_PIN_SIZE.HEIGHT + 'px';
+  };
+
+  /**
+   * Возвращает число в заданные границы
+   * @param {number} number
+   * @param {number} limitMin
+   * @param {number} limitMax
+   * @return {number}
+   */
+  var checkLimit = function (number, limitMin, limitMax) {
+    return Math.min(Math.max(number, limitMin), limitMax);
+  };
+
+  pinMain.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var shift = {
+      x: evt.clientX - pinMain.offsetLeft,
+      y: evt.clientY - pinMain.offsetTop
+    };
+
+    var mouseMoveEventHandler = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      var newX = checkLimit(startCoords.x - shift.x + MAIN_PIN_SIZE.WIDTH / 2, LOCATION.X.MIN, LOCATION.X.MAX);
+      var newY = checkLimit(startCoords.y - shift.y + MAIN_PIN_SIZE.HEIGHT, LOCATION.Y.MIN, LOCATION.Y.MAX);
+
+      setPinCoords(newX, newY);
+      syncFieldWithPin(newX, newY);
+    };
+
+    var mouseUpEventHandler = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', mouseMoveEventHandler);
+      document.removeEventListener('mouseup', mouseUpEventHandler);
+    };
+
+    document.addEventListener('mousemove', mouseMoveEventHandler);
+    document.addEventListener('mouseup', mouseUpEventHandler);
+  });
+
+  addressField.addEventListener('keyup', syncPinWithField);
 
 })();
